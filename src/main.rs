56bf0b19a7412d6ef658dc977725d443f02f3aea
Vitold S <vit1251@gmail.com>
2023-12-20 05:27:14 +0300

    Add Telegraf reporting

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,83 @@
 
+use std::env;
+
 use log::LevelFilter;
 use env_logger::Builder;
 
+use telegraf::{Client, Point};
+
 use rs_co2mon::AirQualityMonitor;
+use rs_co2mon::AirQulityEvent::AmbientTemperature;
+use rs_co2mon::AirQulityEvent::RelativeConcentration;
 
 fn main() {
 
-    /* Step 1. Initialize report system */
-    Builder::new().filter_level(LevelFilter::Debug).init();
+    let mut enable_telegraf: bool = false;
+    let mut enable_debug: bool = false;
+    let mut verbose: bool = true;
 
-    /* Step 2. Create Air Quality Monitor */
+    let mut c: Option<Client> = None;
+
+    /* Step 1. Parse arguments*/
+    for argument in env::args() {
+        if argument == "--debug" {
+            enable_debug = true;
+        }
+        if argument == "--telegraf" {
+            enable_telegraf = true;
+            c = Some(Client::new("tcp://localhost:8094").unwrap());
+        }
+        if argument == "--help" {
+            // TODO - write help message...
+        }
+    }
+
+    /* Step 2. Initialize report system */
+    if enable_debug {
+        Builder::new().filter_level(LevelFilter::Debug).init();
+    }
+
+    /* Step 3. Create Air Quality Monitor */
     let mut air_mon = AirQualityMonitor::new();
     air_mon.open(); //.unwrap();
 
     for event in air_mon {
-        println!("event = {:?}", event);
+        if let Some(ref mut conn) = c {
+            match event {
+                AmbientTemperature { temp } => {
+                    let p = Point::new(
+                        String::from("co2monitor"),
+                        vec![
+//                            (String::from("name"), String::from(""))
+                        ],
+                        vec![
+                            (String::from("ambient_temperature"), Box::new(temp)),
+                        ],
+                        None,
+                    );
+                    conn.write_point(&p).unwrap();
+                },
+                RelativeConcentration { value } => {
+                    let p = Point::new(
+                        String::from("co2monitor"),
+                        vec![
+//                            (String::from("name"), String::from("relative_concentration"))
+                        ],
+                        vec![
+                            (String::from("relative_concentration"), Box::new(value)),
+                        ],
+                        None,
+                    );
+                    conn.write_point(&p).unwrap();
+                },
+                _ => {
+
+                },
+            }
+        }
+        if verbose {
+            println!("event = {:?}", event);
+        }
     }
 
 }
